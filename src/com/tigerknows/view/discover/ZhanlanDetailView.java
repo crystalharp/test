eb397d918a94ee452d5bebef912f1ac64708781a
JiangShuai <jiangshuai@tigerknows.com>
2013-03-27 17:46:18 +0800

    Fixed: Cancel the previous task if exists when a new task want to go.

--- src/com/tigerknows/view/discover/ZhanlanDetailView.java
+++ src/com/tigerknows/view/discover/ZhanlanDetailView.java
@@ -170,6 +170,9 @@ public class ZhanlanDetailView extends BaseDetailView implements View.OnClickLis
             criteria.put(DataOperation.SERVER_PARAMETER_DATA_UID, mData.getUid());
             criteria.put(DataOperation.SERVER_PARAMETER_NEED_FEILD, Util.byteToHexString(Zhanlan.FIELD_DESCRIPTION));
             dataOperation.setup(criteria, Globals.g_Current_City_Info.getId(), mParentFragment.getId(), mParentFragment.getId(), null, true);
+            if(mTKAsyncTasking!=null){
+                mTKAsyncTasking.stop();	
+            }
             mTKAsyncTasking = mSphinx.queryStart(dataOperation);
             mBaseQuerying = mTKAsyncTasking.getBaseQueryList();
         }
@@ -305,6 +308,9 @@ public class ZhanlanDetailView extends BaseDetailView implements View.OnClickLis
                 Util.byteToHexString(Zhanlan.FIELD_PICTURES_DETAIL)+":"+Globals.getPicWidthHeight(TKConfig.PICTURE_DIANYING_DETAIL)+"_[0]");
         criteria.put(BaseQuery.RESPONSE_CODE_ERROR_MSG_PREFIX + 410, ""+R.string.response_code_410_pulled);
         dataOperation.setup(criteria, Globals.g_Current_City_Info.getId(), mParentFragment.getId(), mParentFragment.getId(), mSphinx.getString(R.string.doing_and_wait));
+        if(mTKAsyncTasking!=null){
+            mTKAsyncTasking.stop();	
+        }
         mTKAsyncTasking = mSphinx.queryStart(dataOperation);
         mBaseQuerying = mTKAsyncTasking.getBaseQueryList();
     }
