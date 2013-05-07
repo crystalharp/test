fb505911a25579b8fa43887fd70a1e24b7217889
JiangShuai <jiangshuai@tigerknows.com>
2013-05-07 17:45:58 +0800

    Added: 酒店订单的类。

---
+++ src/com/tigerknows/model/HotelOrder.java
@@ -0,0 +1,356 @@
+package com.tigerknows.model;
+
+import com.decarta.android.exception.APIException;
+import com.decarta.android.location.Position;
+import com.tigerknows.model.xobject.XMap;
+import com.tigerknows.util.ByteUtil;
+
+/**
+ * 
+ * @author jiangshuai
+ *
+ */
+public class HotelOrder extends XMapData{
+	
+	// identification info
+	/**
+	 * 订单的id。
+	 * 创建订单的时候获取的。
+	 */
+	private String id;
+	/**
+	 * 订单创建时间
+	 */
+	private long createTime;
+	/**
+	 * 状态 
+	 * range(1-5)
+	 */
+	private int state = -1;
+
+	// hotel info
+	/**
+	 * 酒店的poi的uuid
+	 * 注意不是酒店动态poi的id
+	 */
+	private String hotelPoiUUID; 
+	/**
+	 * 酒店的名字
+	 */
+	private String hotelName;
+	/**
+	 * 酒店的地址
+	 */
+	private String hotelAddress;
+	/**
+	 * 酒店的位置
+	 */
+	private Position position;
+	/**
+	 * 酒店的电话
+	 * 逗号分隔
+	 */
+	private String hotelTel;
+	
+	// room info
+	/**
+	 * 房型
+	 */
+	private String roomType;
+	/**
+	 * 房间数目
+	 */
+	private String roomNum;
+	
+	// payment info
+	/**
+	 * 总价
+	 */
+	private String totalFee;
+	/**
+	 * 担保类型
+	 */
+	private String guaranteeType;
+
+	// time info
+	/**
+	 * 保留时间
+	 */
+	private long retentionTime;
+	/**
+	 * 入住时间
+	 */
+	private long checkinTime;
+	/**
+	 * 离店时间
+	 */
+	private long checkoutTime;
+
+	// guest info
+	/**
+	 * 入住人姓名
+	 */
+	private String guestName;
+	/**
+	 * 手机号码
+	 */
+	private String mobileNum;
+
+	public static final byte FIELD_ID = 0x01;
+	public static final byte FIELD_CREATE_TIME = 0x02;
+	public static final byte FIELD_STATE = 0x03;
+	public static final byte FIELD_POIUUID = 0x04;
+	public static final byte FIELD_HOTEL_NAME = 0x05;
+	public static final byte FIELD_HOTEL_ADDRESS = 0x06;
+	public static final byte FIELD_POSITION_X = 0x07;
+	public static final byte FIELD_POSITION_Y = 0x08;
+	public static final byte FIELD_HOTEL_TEL = 0x09;
+	public static final byte FIELD_ROOM_TYPE = 0x0A;
+	public static final byte FIELD_ROOM_NUM = 0x0B;
+	public static final byte FIELD_TOTAL_FEE = 0x0C;
+	public static final byte FIELD_GARANTEE_TYPE = 0x0D;
+	public static final byte FIELD_RETENTION_TIME = 0x0E;
+	public static final byte FIELD_CHECKIN_TIME = 0x0F;
+	public static final byte FIELD_CHECKOUT_TIME = 0x10;
+	public static final byte FIELD_GUEST_NAME = 0x11;
+	public static final byte FIELD_MOBILE_NUM= 0x12;
+	
+	/**
+	 * 使用XMap构建酒店订单
+	 * XMap必须存在所有的域否则报APIException
+	 * @param data
+	 * @throws APIException
+	 */
+	public HotelOrder(XMap data) throws APIException{
+		super(data);
+		id = data.getString(FIELD_ID);
+		createTime = data.getInt(FIELD_CREATE_TIME);
+		state = (int) data.getInt(FIELD_STATE);
+		hotelPoiUUID = data.getString(FIELD_POIUUID);
+		hotelName = data.getString(FIELD_HOTEL_NAME);
+		hotelAddress = data.getString(FIELD_HOTEL_ADDRESS);
+		position = new Position(data.getDouble(FIELD_POSITION_Y), data.getDouble(FIELD_POSITION_X));
+		hotelTel = data.getString(FIELD_HOTEL_TEL);
+		roomType = data.getString(FIELD_ROOM_TYPE);
+		totalFee = data.getString(FIELD_TOTAL_FEE);
+		guaranteeType = data.getString(FIELD_GARANTEE_TYPE);
+		retentionTime = data.getInt(FIELD_RETENTION_TIME);
+		checkinTime = data.getInt(FIELD_CHECKIN_TIME);
+		checkoutTime = data.getInt(FIELD_CHECKOUT_TIME);
+		guestName = data.getString(FIELD_GUEST_NAME);
+		mobileNum = data.getString(FIELD_MOBILE_NUM);
+	}
+	
+	/**
+	 * 把酒店订单转化成XMap
+	 * 所有的域必须存在并合法，否则报APIExecption
+	 * @return
+	 * @throws APIException
+	 */
+	public XMap toXMap() throws APIException{
+		XMap map = new XMap();
+		if (id!=null) {
+			map.put(FIELD_ID, id);
+		}else{
+			throw new APIException("FIELD_ID");
+		}
+
+		if (createTime > 0) {
+			map.put(FIELD_CREATE_TIME, createTime);
+		}else{
+			throw new APIException("FIELD_CREATE_TIME");
+		}
+
+		if (state>=1 && state<=5) {
+			map.put(FIELD_STATE, state);
+		}else{
+			throw new APIException("FIELD_STATE");
+		}
+
+		if (hotelPoiUUID!=null) {
+			map.put(FIELD_POIUUID, hotelPoiUUID);
+		}else{
+			throw new APIException("FIELD_POIUUID");
+		}
+
+		if (hotelName!=null) {
+			map.put(FIELD_HOTEL_NAME, hotelName);
+		}else{
+			throw new APIException("FIELD_HOTEL_NAME");
+		}
+
+		if (hotelAddress!=null) {
+			map.put(FIELD_HOTEL_ADDRESS, hotelAddress);
+		}else{
+			throw new APIException("FIELD_HOTEL_ADDRESS");
+		}
+
+		if (position!=null) {
+			map.put(FIELD_POSITION_X, position.getLon());
+			map.put(FIELD_POSITION_Y, position.getLat());
+		}else{
+			throw new APIException("FIELD_POSITION");
+		}
+
+		if (hotelTel!=null) {
+			map.put(FIELD_HOTEL_TEL, hotelTel);
+		}else{
+			throw new APIException("FIELD_HOTEL_TEL");
+		}
+
+		if (roomType!=null) {
+			map.put(FIELD_ROOM_TYPE, roomType);
+		}else{
+			throw new APIException("FIELD_ROOM_TYPE");
+		}
+
+		if (totalFee!=null) {
+			map.put(FIELD_TOTAL_FEE, totalFee);
+		}else{
+			throw new APIException("FIELD_TOTAL_FEE");
+		}
+
+		if (guaranteeType!=null) {
+			map.put(FIELD_GARANTEE_TYPE, guaranteeType);
+		}else{
+			throw new APIException("FIELD_GARANTEE_TYPE");
+		}
+
+		if (retentionTime>0) {
+			map.put(FIELD_RETENTION_TIME, retentionTime);
+		}else{
+			throw new APIException("FIELD_RETENTION_TIME");
+		}
+
+		if (checkinTime>0) {
+			map.put(FIELD_CHECKIN_TIME, checkinTime);
+		}else{
+			throw new APIException("FIELD_CHECKIN_TIME");
+		}
+
+		if (checkoutTime>0) {
+			map.put(FIELD_CHECKOUT_TIME, checkoutTime);
+		}else{
+			throw new APIException("FIELD_CHECKOUT_TIME");
+		}
+
+		if (guestName!=null) {
+			map.put(FIELD_GUEST_NAME, guestName);
+		}else{
+			throw new APIException("FIELD_GUEST_NAME");
+		}
+
+		if (mobileNum!=null) {
+			map.put(FIELD_MOBILE_NUM, mobileNum);
+		}else{
+			throw new APIException("FIELD_MOBILE_NUM");
+		}
+
+		return map;
+		
+	}// end toXMap
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public long getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(long createTime) {
+		this.createTime = createTime;
+	}
+	public int getState() {
+		return state;
+	}
+	public void setState(int state) {
+		this.state = state;
+	}
+	public String getHotelPoiUUID() {
+		return hotelPoiUUID;
+	}
+	public void setHotelPoiUUID(String hotelPoiUUID) {
+		this.hotelPoiUUID = hotelPoiUUID;
+	}
+	public String getHotelName() {
+		return hotelName;
+	}
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+	public String getHotelAddress() {
+		return hotelAddress;
+	}
+	public void setHotelAddress(String hotelAddress) {
+		this.hotelAddress = hotelAddress;
+	}
+	public Position getPosition() {
+		return position;
+	}
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+	public String getHotelTel() {
+		return hotelTel;
+	}
+	public void setHotelTel(String hotelTel) {
+		this.hotelTel = hotelTel;
+	}
+	public String getRoomType() {
+		return roomType;
+	}
+	public void setRoomType(String roomType) {
+		this.roomType = roomType;
+	}
+	public String getRoomNum() {
+		return roomNum;
+	}
+	public void setRoomNum(String roomNum) {
+		this.roomNum = roomNum;
+	}
+	public String getTotalFee() {
+		return totalFee;
+	}
+	public void setTotalFee(String totalFee) {
+		this.totalFee = totalFee;
+	}
+	public String getGuaranteeType() {
+		return guaranteeType;
+	}
+	public void setGuaranteeType(String guaranteeType) {
+		this.guaranteeType = guaranteeType;
+	}
+	public long getRetentionTime() {
+		return retentionTime;
+	}
+	public void setRetentionTime(long retentionTime) {
+		this.retentionTime = retentionTime;
+	}
+	public long getCheckinTime() {
+		return checkinTime;
+	}
+	public void setCheckinTime(long checkinTime) {
+		this.checkinTime = checkinTime;
+	}
+	public long getCheckoutTime() {
+		return checkoutTime;
+	}
+	public void setCheckoutTime(long checkoutTime) {
+		this.checkoutTime = checkoutTime;
+	}
+	public String getGuestName() {
+		return guestName;
+	}
+	public void setGuestName(String guestName) {
+		this.guestName = guestName;
+	}
+	public String getMobileNum() {
+		return mobileNum;
+	}
+	public void setMobileNum(String mobileNum) {
+		this.mobileNum = mobileNum;
+	}
+
+}
