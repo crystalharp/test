5b0bcc6a95178ad253a87f7f2da21ab4fc35dd37
fengtianxiao <fengtianxiao@tigerknows.com>
2013-05-09 18:40:14 +0800

    Fixed: 改版本号 4.40.20130509A
    Signed-off-by: fengtianxiao <fengtianxiao@tigerknows.com>

--- src/com/tigerknows/TKConfig.java
+++ src/com/tigerknows/TKConfig.java
@@ -165,17 +165,17 @@ public class TKConfig {
     /**
      * 网络请求参数中的客户端软件版本
      */
-    private static String sCLIENT_SOFT_VERSION = "4.30.20130326A";
+    private static String sCLIENT_SOFT_VERSION = "4.40.20130509A";
     
     /**
      * 关于我们界面中的客户端软件发布版本
      */
     
-    public static final String CLIENT_SOFT_VERSION = "4.30.alpha1";
+    public static final String CLIENT_SOFT_VERSION = "4.40.alpha1";
     /**
      * 关于我们界面中的客户端软件发布日期
      */
-    public static final String CLIENT_SOFT_RELEASE_DATE = "2013-03-26";
+    public static final String CLIENT_SOFT_RELEASE_DATE = "2013-05-09";
     
     /**
      * 手机产品信息，包括品牌、型号、软件版本等信息
