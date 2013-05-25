e6d2b49a5ac74d5a6cf0cc8e355ee1d497fb2a68
xupeng <xupeng@tigerknows.com>
2013-05-25 17:24:05 +0800

    add:快速注释掉所有Log的脚本

---
+++ scripts/disable_log.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#set -x
+
+######################################################
+# written by xupeng (xupeng@tigerknows.com)
+# comment all the LogWrapper lines in java files
+######################################################
+
+yes=$2
+comment_tag="by_xp"
+cd $1
+
+usage(){
+	echo "usage: ./disable_log.sh src/ -y"
+	echo "       ./disable_log.sh src/ -n"
+}
+
+process_dir(){
+	for i in `ls`
+	do
+		if [ -d $i ]
+		then
+			cd $i;
+			process_dir;
+			cd ..;
+		elif [ -f $i ] && [ ${i##*.} = "java" ]
+		then
+			if [ $yes = "-y" ]
+			then
+				sed -i "s|^ *LogWrapper|//$comment_tag\0|" $i;
+			elif [ $yes = "-n" ]
+			then
+				sed -i "s|^\( *\)//$comment_tag\( *LogWrapper\)|\1\2|" $i;
+			fi
+		fi
+	done
+}
+
+if [ $# -ne 2 ]
+then
+	usage;
+	exit 0;
+fi
+
+process_dir;
