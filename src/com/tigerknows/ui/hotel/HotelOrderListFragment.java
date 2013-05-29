c733057098728b29785fa539d1ba1bf4668c465b
jiangshuai <jiangshuai@tigerknows.com>
2013-05-29 15:51:40 +0800

    Fixed: 列表界面和详情界面价格显示。

--- src/com/tigerknows/ui/hotel/HotelOrderListFragment.java
+++ src/com/tigerknows/ui/hotel/HotelOrderListFragment.java
@@ -292,7 +292,7 @@ public class HotelOrderListFragment extends BaseFragment implements View.OnClick
             TextView dayCountView = (TextView) view.findViewById(R.id.day_count_txv);
 
             nameTxv.setText(order.getHotelName());
-            priceTxv.setText("" + ((int)order.getTotalFee()) );
+            priceTxv.setText(Utility.formatHotelPrice(order.getTotalFee()) + mContext.getString(R.string.rmb_text) );
             roomTypeTxv.setText(order.getRoomType());
             checkinDateTxv.setText(formatOrderListItemMonthDay(order.getCheckinTime() ) );
             checkoutDateTxv.setText(formatOrderListItemMonthDay(order.getCheckoutTime() ) );
