34c4f2cb1a80be8cce5a2a7adb8a7be86e79e1be
pengwenyue <pengwenyue@tigerknows.com>
2013-05-30 16:57:52 +0800

    Fixed:在选择位置界面提示找不到目标位置

--- src/com/tigerknows/model/DataQuery.java
+++ src/com/tigerknows/model/DataQuery.java
@@ -2111,7 +2111,7 @@ public final class DataQuery extends BaseQuery {
         // 0x03    x_int   关键字命中的地片筛选下标 
         public static final byte FIELD_POSITION = 0x03;
 
-        protected long position = -1;
+        protected long position = Long.MIN_VALUE;
         
         protected AlternativeList list;
 
@@ -2127,7 +2127,7 @@ public final class DataQuery extends BaseQuery {
             super(data);
 
             this.list = getObjectFromData(FIELD_LIST, AlternativeList.Initializer);
-            this.position = getLongFromData(FIELD_POSITION);
+            this.position = getLongFromData(FIELD_POSITION, Long.MIN_VALUE);
         }
 
         public static class AlternativeList extends BaseList {
