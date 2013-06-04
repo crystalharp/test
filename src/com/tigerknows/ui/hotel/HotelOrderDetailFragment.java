564eac01ce561bd5a31b212b5edf40d4a5a2374b
jiangshuai <jiangshuai@tigerknows.com>
2013-06-04 10:29:36 +0800

    Fixed: 酒店订单详情页酒店名称栏多行显示不打点。

--- src/com/tigerknows/ui/hotel/HotelOrderDetailFragment.java
+++ src/com/tigerknows/ui/hotel/HotelOrderDetailFragment.java
@@ -357,6 +357,7 @@ public class HotelOrderDetailFragment extends BaseFragment implements View.OnCli
     		return;
     	}
     	
+    	mHotelAddressTxv.setSingleLine(false);
     	DiscoverChildListFragment.showPOI(mContext, order.getHotelName(), null, order.getHotelAddress(), mOrder.getHotelTel(), mHotelNameTxv, mDistanceTxv, 
     			mRootView.findViewById(R.id.address_view), mRootView.findViewById(R.id.divider_imv), mRootView.findViewById(R.id.telephone_view)
     			, mHotelAddressTxv, mHotelTelTxv, R.drawable.list_header, R.drawable.list_footer, R.drawable.list_footer);
