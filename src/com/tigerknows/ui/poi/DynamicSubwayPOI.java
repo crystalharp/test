9c04c3ae5084cc971d76e85de662597e12acf77b
xupeng <xupeng@tigerknows.com>
2013-07-21 11:01:04 +0800

    tmp commit

---
+++ src/com/tigerknows/ui/poi/DynamicSubwayPOI.java
@@ -0,0 +1,35 @@
+package com.tigerknows.ui.poi;
+
+import java.util.List;
+
+import com.tigerknows.android.os.TKAsyncTask;
+import com.tigerknows.ui.poi.POIDetailFragment.DynamicPOIView;
+import com.tigerknows.ui.poi.POIDetailFragment.DynamicPOIViewBlock;
+
+public class DynamicSubwayPOI extends DynamicPOIView {
+
+    @Override
+    public void onPostExecute(TKAsyncTask tkAsyncTask) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onCancelled(TKAsyncTask tkAsyncTask) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public List<DynamicPOIViewBlock> getViewList() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void refresh() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
