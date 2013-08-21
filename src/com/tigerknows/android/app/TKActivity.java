94d785fc40b396cb5e4f1dcad251f63199763346
pengwenyue <pengwenyue@tigerknows.com>
2013-08-21 13:47:37 +0800

    Fixed:And5.50-24【交通】公交方案，分享到新浪微博和QQ空间，点击发送后，提示语中缺少省略号。

--- src/com/tigerknows/android/app/TKActivity.java
+++ src/com/tigerknows/android/app/TKActivity.java
@@ -404,7 +404,7 @@ public class TKActivity extends MapActivity implements TKAsyncTask.EventListener
             dialog = new ProgressDialog(this);
             dialog.setCancelable(false);
             dialog.setCanceledOnTouchOutside(false);
-            ((ProgressDialog)dialog).setMessage(getString(R.string.doing_and_wait));
+            ((ProgressDialog)dialog).setMessage(getString(R.string.doing_and_wait)+"......");
             dialog.setOnDismissListener(new OnDismissListener() {
                 
                 @Override
