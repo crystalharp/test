a151b23f8ecf961e0992a00ce25e3231e0a1a3d1
xupeng <xupeng@tigerknows.com>
2013-10-12 16:38:25 +0800

    add:有地铁的城市开机20秒后自动下载地铁图

---
+++ src/com/tigerknows/StartUpDelayRunnable.java
@@ -0,0 +1,27 @@
+package com.tigerknows;
+
+import com.decarta.Globals;
+import com.tigerknows.map.MapEngine;
+import com.tigerknows.model.FileDownload;
+
+public class StartUpDelayRunnable implements Runnable{
+
+    Sphinx mSphinx;
+    
+    public StartUpDelayRunnable(Sphinx sphinx) {
+        mSphinx = sphinx;
+    }
+    
+    @Override
+    public void run() {
+        
+        if (MapEngine.checkSupportSubway(Globals.getCurrentCityInfo().getId()) &&
+                "WIFI".equals(TKConfig.getConnectivityType(mSphinx))) {
+            FileDownload fileDownload = new FileDownload(mSphinx);
+            fileDownload.addParameter(FileDownload.SERVER_PARAMETER_FILE_TYPE, FileDownload.FILE_TYPE_SUBWAY);
+            fileDownload.setup(Globals.getCurrentCityInfo().getId());
+            mSphinx.queryStart(fileDownload);
+        }
+    }
+
+}
