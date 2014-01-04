c7a0fd025e34b3ab29875b810426ea1aa03abece
pengwenyue <pengwenyue@tigerknows.com>
2014-01-04 10:37:18 +0800

    Fixed: 	And5.8.0-136【长按选点】在地图上长按空白区，获取到附近poi应显示：XXX附近，现在不显示‘附近’2字

--- src/com/tigerknows/ui/InfoWindowFragment.java
+++ src/com/tigerknows/ui/InfoWindowFragment.java
@@ -391,15 +391,28 @@ public class InfoWindowFragment extends BaseFragment implements View.OnClickList
             mSphinx.setMapViewPaddingBottom(mHeight);
             
             View view = (View) mCyclePagerAdapter.viewList.get((position) % mCyclePagerAdapter.viewList.size());
-            setMessageToView(view, ((POI) mItemizedOverlay.get(position).getAssociatedObject()).getName(), null, true);
+            String name = ((POI) mItemizedOverlay.get(position).getAssociatedObject()).getName();
+            String selectPoint = getString(R.string.select_point);
+            if (selectPoint.equals(name) == false) {
+            	name += getString(R.string.nearby);
+            }
+            setMessageToView(view, name, null, true);
             
             if (prevPosition != -1) {
                 view = mCyclePagerAdapter.viewList.get(prevPosition % mCyclePagerAdapter.viewList.size());
-                setMessageToView(view, ((POI) mItemizedOverlay.get(prevPosition).getAssociatedObject()).getName(), null, true);
+                name = ((POI) mItemizedOverlay.get(prevPosition).getAssociatedObject()).getName();
+                if (selectPoint.equals(name) == false) {
+                	name += getString(R.string.nearby);
+                }
+                setMessageToView(view, name, null, true);
             }
             if (nextPosition != -1) {
                 view = mCyclePagerAdapter.viewList.get(nextPosition % mCyclePagerAdapter.viewList.size());
-                setMessageToView(view, ((POI) mItemizedOverlay.get(nextPosition).getAssociatedObject()).getName(), null, true);
+                name = ((POI) mItemizedOverlay.get(nextPosition).getAssociatedObject()).getName();
+                if (selectPoint.equals(name) == false) {
+                	name += getString(R.string.nearby);
+                }
+                setMessageToView(view, name, null, true);
             }
         } else if (mType == TYPE_MAP_POI) {
 
